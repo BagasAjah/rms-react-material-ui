1de4eb5b9cb369e4bfdd3afba4419ddfbc892b3e
BagasAjah <BagasDimas.Permadi@mitrais.com>
2017-02-22 15:10:26 +0700

    fix missing properties

--- src/component/containers/employee/EmployeeTabHistory.js
+++ src/component/containers/employee/EmployeeTabHistory.js
@@ -6,6 +6,7 @@ import { changeEditEmployees, changeNewEmployee, changeOpenDialogValue } from ".
 
 const mapStateToProps = (state, props) => ({
     currentEmployee: props.currentEmployee,
+    newEmployee: state.newEmployee,
     openDialog: state.openDialog,
     pageMode: props.pageMode,
     viewMode: props.viewMode
